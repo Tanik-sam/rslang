1ef9d4b43c3e33c0b28227c2b131f609b110f274
Semion Krapivin <homer1996@gmail.com>
2022-08-22 04:10:01 +0300

    feat: add start screen for sprint game

---
+++ src/view/sprint/sprint.ts
@@ -0,0 +1,23 @@
+import '../../style.scss';
+import '../../assets/sass/components/_sprint.scss';
+import Welcome from './welcome';
+// import { getWords } from '../../controller/fetch';
+
+class SprintGame {
+    private welcome: Welcome;
+
+    constructor() {
+        this.welcome = new Welcome();
+    }
+
+    drawWelcome() {
+        this.welcome.drawDefault();
+    }
+}
+
+window.onload = function SprintGameInit(): void {
+    const sprintGame: SprintGame = new SprintGame();
+    sprintGame.drawWelcome();
+};
+
+export default SprintGame;
