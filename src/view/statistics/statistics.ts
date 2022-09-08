bb4b17258208e4597e81a8741dbee54daa61f2f0
Tatiana <tanik.sam@yandex.ru>
2022-09-08 20:51:45 +0300

    fix: fix adoptive design for statistics

--- src/view/statistics/statistics.ts
+++ src/view/statistics/statistics.ts
@@ -14,41 +14,62 @@ class Statistics {
         (async () => {
             if (localStorage.currentUserName && localStorage.currentUserEmail) {
                 this.userStatistics = await getUserStatistics();
-            }
-            try {
-                (document.querySelector(
-                    '#statAudioLearned'
-                ) as HTMLElement).innerHTML = `${this.userStatistics.learnedWords}`;
-                (document.querySelector('#statAudioSuc') as HTMLElement).innerHTML = `${
-                    this.userStatistics.optional.audioSuc || 0 / (this.userStatistics.optional.audioAll || 1)
-                }%`;
-                (document.querySelector(
-                    '#statAudioSer'
-                ) as HTMLElement).innerHTML = `${this.userStatistics.optional.audioSeria}`;
-                (document.querySelector(
-                    '#statSprintLearned'
-                ) as HTMLElement).innerHTML = `${this.userStatistics.learnedWords}`;
-                (document.querySelector('#statSprintSuc') as HTMLElement).innerHTML = `${
-                    this.userStatistics.optional.sprintSuc || 0 / (this.userStatistics.optional.sprintAll || 1)
-                }%`;
-                (document.querySelector(
-                    '#statSprintSer'
-                ) as HTMLElement).innerHTML = `${this.userStatistics.optional.sprintSeria}`;
-                (document.querySelector(
-                    '#statAllLearned'
-                ) as HTMLElement).innerHTML = `${this.userStatistics.learnedWords}`;
-                (document.querySelector('#statAllSuc') as HTMLElement).innerHTML = `${Math.round(
-                    (this.userStatistics.optional.sprintSuc || 0) +
-                        (this.userStatistics.optional.audioSuc || 0) /
-                            ((this.userStatistics.optional.sprintAll || 0) + this.userStatistics.optional.audioAll || 1)
-                )}%`;
-                (document.querySelector('#statAllSer') as HTMLElement).innerHTML = `${
-                    (this.userStatistics.optional.sprintSeria || 0) + (this.userStatistics.optional.audioSeria || 0)
-                }`;
-            } catch (e) {
-                throw new Error(`Error`);
+                try {
+                    (document.querySelector(
+                        '#statAudioLearned'
+                    ) as HTMLElement).innerHTML = `${this.userStatistics.learnedWords}`;
+                    (document.querySelector('#statAudioSuc') as HTMLElement).innerHTML = `${
+                        this.userStatistics.optional.audioSuc || 0 / (this.userStatistics.optional.audioAll || 1)
+                    }%`;
+                    (document.querySelector(
+                        '#statAudioSer'
+                    ) as HTMLElement).innerHTML = `${this.userStatistics.optional.audioSeria}`;
+                    (document.querySelector(
+                        '#statSprintLearned'
+                    ) as HTMLElement).innerHTML = `${this.userStatistics.learnedWords}`;
+                    (document.querySelector('#statSprintSuc') as HTMLElement).innerHTML = `${
+                        this.userStatistics.optional.sprintSuc || 0 / (this.userStatistics.optional.sprintAll || 1)
+                    }%`;
+                    (document.querySelector(
+                        '#statSprintSer'
+                    ) as HTMLElement).innerHTML = `${this.userStatistics.optional.sprintSeria}`;
+                    (document.querySelector(
+                        '#statAllLearned'
+                    ) as HTMLElement).innerHTML = `${this.userStatistics.learnedWords}`;
+                    (document.querySelector('#statAllSuc') as HTMLElement).innerHTML = `${Math.round(
+                        (this.userStatistics.optional.sprintSuc || 0) +
+                            (this.userStatistics.optional.audioSuc || 0) /
+                                ((this.userStatistics.optional.sprintAll || 0) +
+                                    this.userStatistics.optional.audioAll || 1)
+                    )}%`;
+                    (document.querySelector('#statAllSer') as HTMLElement).innerHTML = `${
+                        (this.userStatistics.optional.sprintSeria || 0) + (this.userStatistics.optional.audioSeria || 0)
+                    }`;
+                } catch (e) {
+                    throw new Error(`Error`);
+                }
+            } else {
+                this.modal();
             }
         })();
+    }
+
+    modal() {
+        const fragment = document.createDocumentFragment() as DocumentFragment;
+        const modal = document.querySelector('#modal') as HTMLTemplateElement;
+        const userClone = modal.content.cloneNode(true) as HTMLElement;
+        fragment.append(userClone);
+        (document.querySelector('.statistic__container--main') as HTMLElement).append(fragment);
+        (document.querySelector('#exit') as HTMLElement).addEventListener('click', () => {
+            (document.querySelector('.statistic__container--main') as HTMLElement).removeChild(
+                document.querySelector('.overlay') as HTMLElement
+            );
+        });
+        (document.querySelector('#exit2') as HTMLElement).addEventListener('click', () => {
+            (document.querySelector('.statistic__container--main') as HTMLElement).removeChild(
+                document.querySelector('.overlay') as HTMLElement
+            );
+        });
     }
 }
 
